1bd361af603c43959f7f26fcde58db2f9d692a87
Yoav Ganbar <yoav.ganbar@gmail.com>
2021-02-09 07:12:14 +0200

    Install and setup Tailwind.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import './tailwind.css';
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   /** custom content, defaults to 'the snozzberries taste like snozzberries' */
@@ -11,5 +12,9 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
  * A custom Thing component. Neat!
  */
 export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+  return (
+    <div className="flex items-center justify-center w-5/6 m-auto text-2xl text-center text-pink-700 uppercase bg-blue-300 shadow-xl rounded-3xl">
+      {children || `the snozzberries taste like snozzberries`}
+    </div>
+  );
 };
