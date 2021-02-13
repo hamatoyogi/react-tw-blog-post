e6259429b622365c8959915a5eaaefabfb8d4bef
Yoav Ganbar <yoav.ganbar@gmail.com>
2021-02-13 23:06:22 +0200

    Add Chromatic. Make change to Thing Compnent for Testing Chromatic.

--- src/Thing.tsx
+++ src/Thing.tsx
@@ -13,7 +13,7 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
  */
 export const Thing: FC<Props> = ({ children }) => {
   return (
-    <div className="flex items-center justify-center w-5/6 m-auto text-2xl text-center text-pink-700 uppercase bg-blue-300 shadow-xl rounded-3xl">
+    <div className="flex items-center justify-center w-5/6 m-auto text-2xl text-center text-pink-700 uppercase bg-yellow-400 shadow-xl rounded-3xl">
       {children || `the snozzberries taste like snozzberries`}
     </div>
   );
