f11da29932bea39f6dc8b3fb851bdd801ecb36ee
Yoav Ganbar <yoav.ganbar@gmail.com>
2021-02-13 08:13:11 +0200

    Add styled componets TW example.

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
-const StyledButton = styled.button`
-  background-color: blue;
-  color: white;
-`;
+const StyledButton = styled.button.attrs(
+  ({ variant }: { variant: ButtonVariants }) => ({
+    className: `px-8 py-2 font-semibold ${
+      variant === 'default' ? 'text-white' : 'text-red-700'
+    } transition duration-500 ease-in-out transform rounded-lg shadow-xl bg-gradient-to-r from-red-300 to-blue-300 hover:from-pink-400 hover:to-indigo-400`,
+  })
+)<{ variant: ButtonVariants }>``;
 
+type ButtonVariants = 'default' | 'warning';
 export interface ButtonProps {
   text: string;
+  variant?: ButtonVariants;
 }
 
-export const Button: FC<ButtonProps> = ({ text }) => {
-  return <StyledButton>{text}</StyledButton>;
+export const Button: FC<ButtonProps> = ({ text, variant = 'default' }) => {
+  return <StyledButton variant={variant}>{text}</StyledButton>;
 };
